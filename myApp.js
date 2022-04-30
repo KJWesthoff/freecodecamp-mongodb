71f535bee53b9faeb5b99aa01960a08d057b78bc
KJWesthoff <karl.johan.westhoff@gmail.com>
2022-04-29 20:10:27 -0700

    added env variables for database connection

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-require('mongoose')
+var mongoose = require('mongoose')
 
 
-omngoose.connect(<Your URI>, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 let Person;
 
 const createAndSavePerson = (done) => {
